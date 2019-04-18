8955ae3233e7f5f95ca5ea8c4a48c706119b2ae1
scoslo5512 <robert.queeney@gmail.com>
2019-04-17 18:01:34 -0700

    Got OBS streaming working and in the app

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,10 +1,46 @@
-import React from 'react'; 
+import React from 'react';
+import flv from 'flv.js'; 
 import { connect } from 'react-redux'; 
 import { fetchStream } from '../../actions';
 
 class StreamShow extends React.Component{
+    // need to create constructor in order to get ref to video element on the DOM
+    constructor(props){
+        super(props);
+// the ref to the video 
+        this.videoRef = React.createRef();
+    }
+
+    // the video player specific code can be found in the docs: https://www.npmjs.com/package/flv.js
     componentDidMount(){
-        this.props.fetchStream(this.props.match.params.id)
+        const { id } = this.props.match.params;
+        console.log(this.videoRef);
+
+        this.props.fetchStream(id);
+        this.buildPlayer();
+
+    }
+
+    // when the component fetches the stream sucessfuly, we should call buildplayer again
+    componentDidUpdate() {
+        this.buildPlayer();
+    }
+
+    componentWillUnmount() {
+        this.player.destroy();
+    }
+
+    buildPlayer(){
+        if (this.player || !this.props.stream) {
+            return; 
+        } 
+        const { id } = this.props.match.params;
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
     }
 
     render(){
@@ -16,6 +52,7 @@ class StreamShow extends React.Component{
 
         return(
             <div>
+                <video ref={this.videoRef} style={{ width: '100%'}} controls={true}/>
                 <h1>{title}</h1>
                 <h5>{description}</h5>
             </div>
